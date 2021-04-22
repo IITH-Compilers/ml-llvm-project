07ac34aeaffc6185073c34a4d646330f39817d8f
Rohit Aggarwal <cs18mtech11030@iith.ac.in>
2021-04-23 01:18:43 +0530

    Added the data collections scripts

---
+++ preprocessing/v0/config.sh
@@ -0,0 +1,31 @@
+PWD=`pwd`
+HOME=`realpath ${PWD}/../..`
+
+BUILD_TYPE="_release"
+
+LLVM_BUILD="${HOME}/llvm-project/build${BUILD_TYPE}"
+echo "LLVM Build used for the data generation: ${LLVM_BUILD}"
+[[ ! -d ${LLVM_BUILD} ]] && echo "LLVM build directory does not exist" && exit
+
+INP_DIR=${HOME}/data/SPEC_NEW_UNLINK_Ind_iv_REL_AsrtON
+echo "Input data directory path: ${INP_DIR}"
+[[ ! -d ${INP_DIR} ]] && echo "Input directory does not exist" && exit
+
+LL_FLR_NAME=level-O0-llfiles
+INP_TYPE=llfiles
+
+WD=${INP_DIR}/G_O3_1
+echo "${WD}: Run to check the effect of added new ID if loopid not found in rdg on data"
+
+USE_MCA=
+# USE_MCA=" -use-mca "
+TIME_OUT= # "timeout --kill-after=2m 2m "
+
+
+REMARKS=
+
+OPT_PASSES_SEQ="-O3 "
+
+echo "Pass seq used to generate data : ${SSA_PASSES_SEQ}"
+
+${LLVM_BUILD}/bin/llvm-as < /dev/null | ${LLVM_BUILD}/bin/llc ${OPT_PASSES_SEQ}  -regalloc=mlra -debug-pass=Arguments
---
+++ preprocessing/v0/flow.sh
@@ -0,0 +1,35 @@
+ 
+source config.sh
+
+LL_WD=${WD}/llfiles
+asm_WD=${WD}/asm
+
+mkdir -p ${LL_WD} ${asm_WD}
+
+REMARKS_DIR=${WD}/remarks
+GRAPHS=${WD}/graphs
+
+IG_DOT=${GRAPHS}/IG/dot
+IG_JSON=${GRAPHS}/IG/json
+ASM=${asm_WD}
+
+mkdir -p ${IG_DOT} ${IG_JSON} ${ASM} ${REMARKS_DIR}
+
+# Store the dots file
+
+for d in ${INP_DIR}/${LL_FLR_NAME}/*.ll; do 
+        name=`basename ${d}` && oname=${name%.*} && rfile= && if [ ! -z "${REMARKS}" ]; then rfile="-pass-remarks-output=${oname}_${G_TYPE}.yaml"; fi &&  cd ${IG_DOT} && ${TIME_OUT} ${LLVM_BUILD}/bin/clang ${OPT_PASSES_SEQ} -mllvm -regalloc=mlra ${USE_MCA} -S ${REMARKS} ${rfile}   ${d} -o ${ASM}/${oname}.s & 
+done 
+ 
+wait
+
+echo "============================   Dots file generated in dots folder. ================================="
+mv ${IG_DOT}/*.yaml ${REMARKS_DIR}/
+# Covert dot to json and filter out unwanted json
+
+echo "Graphs are present at ${GRAPHS}/"
+python  Dot-\>Json.py ${GRAPHS}/IG
+
+echo "valid graphs generated inside ${GRAPHS}"
+
+echo "Done with preprocessng ...."
