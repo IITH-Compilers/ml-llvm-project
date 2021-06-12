18634e36e8a8abde26e7529ac8cd971cf27b4ec8
anilavakundu <anilavakundu007@gmail.com>
2021-06-12 14:42:57 +0530

    Adding wrappers for llvm-grpc methods

--- llvm-grpc/Include/gRPCUtil.h
+++ llvm-grpc/Include/gRPCUtil.h
@@ -1,14 +1,27 @@
 #include<memory>
+#include<future>
+#include <grpcpp/grpcpp.h>
 
 class gRPCUtil{
-  public:
-		template <typename Service> 
-    int  RunService();
-    template <typename Service>
-    int  RunService(int argc,char **argv);
-    template <typename Client>
-    int SetStub();
+  public: 
+    int  RunService(grpc::Service *s);
+    template <typename Client> int SetStub() {
+
+  		std::shared_ptr<grpc::Channel> channel =
+      grpc::CreateChannel("0.0.0.0:50051", grpc::InsecureChannelCredentials());
+
+  		auto Stub_temp = Client::NewStub(channel);
+
+  		stub_ = Stub_temp.release();
+
+  		return 0;
+		}
     void* getStub(){ return stub_; }
+  protected:
+    std::promise<void>* exit_requested;
   private:
     void* stub_;
+        
 };
+
+
