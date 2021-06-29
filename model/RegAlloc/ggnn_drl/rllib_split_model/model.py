a16f4fc2928f36a09a91f91fd09eafdcbc22e24a
Siddharth Jain <jsiddharth1008@gmail.com>
2021-06-29 20:41:27 +0530

    WIP rllib split model

---
+++ model/RegAlloc/ggnn_drl/rllib_split_model/model.py
@@ -0,0 +1,201 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import logging
+from ggnn import GatedGraphNeuralNetwork
+from register_action_space import RegisterActionSpace
+
+logger = logging.getLogger(__file__) 
+class SelectTaskNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size,  seed, action_size, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(SelectTaskNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
+
+class SelectNodeNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size,  seed, action_size, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(SelectNodeNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
+
+class ColorNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size,  seed, action_size, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(ColorNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
+
+class SplitNodeNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size,  seed, action_size, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(SplitNodeNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
+
+class QNetwork(TorchModelV2):
+    """Actor (Policy) Model."""
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action - Not used 
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        TorchModelV2.__init__(self, obs_space, action_space, num_outputs,
+                              model_config, name)
+        nn.Module.__init__(self)
+
+        # super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(model_config['seed'])
+        self.registerAS = action_space
+        action_size = self.registerAS.ac_sp_normlize_size
+        
+        self.ggnn = GatedGraphNeuralNetwork(hidden_size=model_config['state_size']+1, annotation_size=3, num_edge_types=1, layer_timesteps=[1], residual_connections={}, nodelevel=True)
+        self.selectNodeNet = SelectNodeNetwork(model_config['state_size']+1, model_config['seed'], 1)
+        self.selectTaskNet = SelectTaskNetwork(model_config['state_size']+1, model_config['seed'], 2)
+        self.colorNet = ColorNetwork(model_config['state_size']+1, model_config['seed'], action_size)
+        self.splitNodeNet = SplitNodeNetwork(model_config['state_size']+1, model_config['seed'], 100)
+
+    def computeNode(self, state):
+         self.hidden_state =  self.ggnn(initial_node_representation=state.initial_node_representation, annotations=state.annotations, adjacency_lists=state.adjacency_lists)
+         select_out = self.selectNodeNet(self.hidden_state)
+         return select_out
+
+    def computeTask(self, state):
+        self.hidden_state =  self.ggnn(initial_node_representation=state.initial_node_representation, annotations=state.annotations, adjacency_lists=state.adjacency_lists)
+        node_index = state.focus
+        task_out = self.selectTaskNet(self.hidden_state[node_index])
+        return task_out
+    
+    def computeColor(self, state):
+        self.hidden_state =  self.ggnn(initial_node_representation=state.initial_node_representation, annotations=state.annotations, adjacency_lists=state.adjacency_lists)
+        node_index = state.focus
+        color_out = self.colorNet(self.hidden_state[node_index])
+        
+        # color_out = color_out.reshape(color_out.shape[0])
+        
+        return color_out
+    
+    def computeSplit(self, state):
+        self.hidden_state =  self.ggnn(initial_node_representation=state.initial_node_representation, annotations=state.annotations, adjacency_lists=state.adjacency_lists)
+        node_index = state.focus
+        split_out = self.splitNodeNet(self.hidden_state[node_index])
+
+        return split_out
+    
+    def forward(self, input_dict, state, seq_lens):
+        """Build a network that maps state -> action values."""
+        # logging.debug('MODEL: forward : ', state.shape, type(state) , state)
+        # initial_node_representation, annotations, adjacency_lists, graph_topology, eligibleNodes = state
+        hidden_state =  self.ggnn(initial_node_representation=state.initial_node_representation, annotations=state.annotations, adjacency_lists=state.adjacency_lists)
+        select_out = self.selectNodeNet(hidden_state)
+        # print('select_out ', select_out)
+        #print('eligibleNodes', state.eligibleNodes)
+        masked_select_out = select_out[state.eligibleNodes]
+        #print('masked ',masked_select_out)
+        rel_indexchoose = torch.argmax(masked_select_out)
+        # select_qvalue = torch.max(select_out)
+        node_index = state.eligibleNodes[rel_indexchoose]
+        
+        task_out = selectTaskNet(hidden_state[node_index])
+        taskchoose = torch.argmax(task_out)
+        task_Qvalue = torch.max(task_out)
+
+        if taskchoose == 0: #Color
+            regclass = state.reg_class_list[node_index]
+            adj_colors = state.graph_topology.getColorOfVisitedAdjNodes(node_index)
+
+            action_space = self.registerAS.maskActionSpace(regclass, adj_colors)
+            if action_space is None or len(action_space) == 0:
+                color_out = None
+            else:
+                color_out = self.colorNet(hidden_state[node_index])
+            # color_out = color_out[action_space]
+        else: #Split
+            split_out = self.splitNodeNet(hidden_state[node_index])
+
+        return select_out, node_index, color_out, action_space
+
---
+++ model/RegAlloc/ggnn_drl/rllib_split_model/src/env.py
@@ -0,0 +1,136 @@
+from ggnn import get_observations
+from topologicalSort import Graph
+import random
+import utils
+import os
+import logging
+import torch
+import copy
+logger = logging.getLogger(__file__) 
+class GraphColorEnv(gym.Env):
+
+    def __init__(self, config):
+        self.spill_color_idx = 0
+        self.action_space = None
+        self.ggnn = None
+        self.graph = None
+        self.topology = None # Have the graph formed from adjency list using dependence edges only.
+        self.cur_node = None
+        self.mode = config.mode
+        self.color_assignment_map = {}
+        self.total_reward = 0
+        
+        
+    def reward_formula(self, value, action):
+        # reward = value
+        
+        reward = 0
+        if action == self.spill_color_idx:
+            reward = -value
+            logging.info('Spill is choosen so rewarded {} to node_id={} with spillcost={}'.format(reward, self.obs.idx_nid[self.cur_node], value))
+        self.total_reward = self.total_reward + reward
+        return reward
+
+
+    def getReward_Static(self, action):
+       
+        method_name = self.functionName
+        ll_file_name = self.fileName
+        
+        # ipc to llvm splill cost function for reward
+
+        spillcost = self.obs.spill_cost_list[self.cur_node]
+        reward = self.reward_formula(spillcost, action)
+
+        return reward
+
+    def getReward(self, action):
+        return self.getReward_Static(action)
+
+
+    def step(self, action):
+        
+        nodeChoosen, reg_allocated = action
+        self.cur_node = nodeChoosen
+        # add the node to the visited list
+        self.obs.graph_topology.UpdateVisitList(nodeChoosen, reg_allocated)
+        
+        self.obs.eligibleNodes.remove(nodeChoosen) 
+        
+        node_id =  self.obs.idx_nid[nodeChoosen]
+        
+        self.color_assignment_map[node_id] = reg_allocated
+
+        logging.debug('Color the node with index={cur_node}, node_id={node_id} with color={action} in RegClass={regclass}'.format(cur_node=nodeChoosen, node_id=node_id, action=reg_allocated, regclass=self.obs.reg_class_list[self.cur_node]))
+        
+
+        if reg_allocated != self.spill_color_idx:
+            self.obs.annotations[nodeChoosen][0] =  torch.tensor(0)
+            self.obs.annotations[nodeChoosen][1] = torch.tensor(reg_allocated)# .to(device)
+       
+        # update the graph representations
+        reward = 0
+        done = False
+        
+        # possible_next_nodes = self.topology.findAllVertaxWithZeroWeights()
+        
+        reward = self.getReward(reg_allocated)
+        response = None 
+        if False not in self.obs.graph_topology.discovered:
+            response = utils.get_colored_graph(self.fun_id, self.fileName, self.functionName, self.color_assignment_map)
+            done = True
+            reward = self.total_reward
+            return copy.deepcopy(self.obs), reward, done, response
+
+        # next_hidden_state = next_hidden_state[possible_next_nodes]
+
+        # self.cur_node = self.cur_node + 1
+        # next_node = self.cur_node 
+        # next_obs = next_hidden_state[next_node]
+        # assert not self.topology.discovered[self.cur_node], 'Node {} already visited so taking next node.'.format(self.cur_node)
+ 
+        # adj_colors = self.topology.getColorOfVisitedAdjNodes(next_node)
+        # regClass = self.reg_class_list[self.cur_node]
+        # logging.debug('Node Choosen for coloring : {}'.format(next_node))
+        # logging.debug('Adjacent colors : {}'.format(adj_colors))
+        # logging.debug('regClass of Nodes : {}'.format(regClass))
+        return copy.deepcopy(self.obs), reward, done, response
+    
+    # input graph : jsonnx
+    # return the state of the graph, all the possible starting nodes
+    def reset_env(self, graph, path=None):
+        self.color_assignment_map = {}
+        self.total_reward = 0
+
+        logging.debug('reset the env.')
+        if path is not None:
+            attr = utils.getllFileAttributes(path)
+            self.path = path
+            self.home_dir = attr['HOME_DIR']
+        
+        self.graph = graph
+        self.fileName = graph['graph'][1][1]['FileName'].strip('\"')
+        self.functionName = graph['graph'][1][1]['Function'].strip('\"')
+        self.fun_id = graph['graph'][1][1]['Function_ID']
+        self.num_nodes = len(self.graph['nodes'])
+        
+        self.obs = get_observations(self.graph)
+         
+        # self.spill_cost_list = self.obs.spill_cost_list
+        # self.reg_class_list = self.obs.reg_class_list
+        # self.topology = self.obs.graph_topology
+        
+        state = self.obs # (self.obs.initial_node_representation, self.obs.annotations, self.obs.adjacency_lists, self.obs.graph_topology, self.obs.eligibleNodes, self.obs.reg_class_list, self.obs.spill_cost_list)
+        # while self.topology.discovered[self.cur_node]:
+        #     logging.debug('Node {} already visited so taking next node.'.format(self.cur_node))
+        #     self.cur_node = self.cur_node+1
+        
+        # obs= hidden_state[self.cur_node]
+        # adj_colors = self.topology.getColorOfVisitedAdjNodes(self.cur_node)
+        # self.regClass = self.reg_class_list[self.cur_node]
+        # logging.debug('Node Choosen for coloring : {}'.format(self.cur_node))
+        # logging.debug('Adjacent colors : {}'.format(adj_colors))
+        # logging.debug('regClass of Nodes : {}'.format(self.regClass))
+        
+        # return ( obs, self.cur_node, adj_colors, self.regClass)
+        return copy.deepcopy(state)
