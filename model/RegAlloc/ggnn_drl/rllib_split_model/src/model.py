bdb5ef413032c75893263ab692a0840e78a2ed91
Umesh-k26 <umeshkalvakuntla26@gmail.com>
2023-04-24 15:39:07 +0530

    Replaced ggnn with torch_geometric.nn.GCN

--- model/RegAlloc/ggnn_drl/rllib_split_model/src/model.py
+++ model/RegAlloc/ggnn_drl/rllib_split_model/src/model.py
@@ -9,7 +9,8 @@ from ray.rllib.models.torch.misc import SlimFC, normc_initializer
 import numpy as np
 from ggnn_1 import get_observations, get_observationsInf, GatedGraphNeuralNetwork, constructVectorFromMatrix, AdjacencyList
 import random
-
+from torch_geometric.nn import GCN
+from torch_geometric.data import Data, Batch
 logger = logging.getLogger(__file__) 
 random.seed(123)
 np.random.seed(123)
@@ -113,8 +114,9 @@ class SelectNodeNetwork(TorchModelV2, nn.Module):
         annotations_size: int = custom_config["annotations_size"]
         self.max_number_nodes = custom_config["max_number_nodes"]
         self.emb_size = custom_config["state_size"]
-        self.ggnn = GatedGraphNeuralNetwork(hidden_size=state_size, annotation_size=annotations_size, num_edge_types=1, layer_timesteps=[5], residual_connections={}, nodelevel=True, batch_norm_param=self.max_number_nodes, max_edge_count=custom_config["max_edge_count"])
+        # self.ggnn = GatedGraphNeuralNetwork(hidden_size=state_size, annotation_size=annotations_size, num_edge_types=1, layer_timesteps=[5], residual_connections={}, nodelevel=True, batch_norm_param=self.max_number_nodes, max_edge_count=custom_config["max_edge_count"])
 
+        self.ggnn = GCN(in_channels=103, hidden_channels=100, out_channels=100, num_layers=2, norm='BatchNorm', dropout=0.2)
         
     def forward(self, input_dict, state, seq_lens):
         """Build a network that maps state -> action values."""
@@ -122,7 +124,52 @@ class SelectNodeNetwork(TorchModelV2, nn.Module):
         # print("State shape:", input_dict["obs"]["state"].shape)
         input_state_list = torch.zeros(input_dict["obs"]["state"].shape[0], self.max_number_nodes, self.emb_size)
         if self.enable_ggnn:
-            node_mat = self.ggnn(initial_node_representation=input_dict["obs"]["state"], annotations=input_dict["obs"]["annotations"], adjacency_lists=input_dict["obs"]["adjacency_lists"])
+            # print("*************************** SHAPES PRINTING *****************************")
+            # print("-----------------BEFORE GGNN-------------")
+
+            # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            # print("from select node forward func : ", device)
+            # print("adjacency list device = ", input_dict["obs"]["adjacency_lists"][0].data.device)
+            # print("annotations device = ", input_dict["obs"]["annotations"].device)
+            assert not torch.isnan(input_dict["obs"]["state"]).any(), "Nan in select node model obs:state"
+            assert not torch.isnan(input_dict['obs']['adjacency_lists']['data'].values).any(), "Nan in select node model adjlist"
+
+
+            ggnn_input_x = torch.dstack((input_dict["obs"]["state"], input_dict["obs"]["annotations"]))
+            edge_index = input_dict['obs']['adjacency_lists']['data'].values.mT
+
+
+
+
+            # print("ggnn_input_x.device = ", ggnn_input_x.device)
+            # print("edge_index.device = ", edge_index.device)
+            batch_size = ggnn_input_x.shape[0]
+            # #################### batch method #######################
+            data_list = []
+
+            data_list = [Data(x=ggnn_input_x[i], edge_index=edge_index[i].long()) for i in range(batch_size)]
+            batch_data = Batch.from_data_list(data_list=data_list)
+
+            node_mat = self.ggnn(batch_data.x, batch_data.edge_index).reshape(batch_size, -1, 100)
+            assert not torch.isnan(node_mat).any(), "Nan in select node model input after ggnn"
+
+
+            # print(batch_data)
+            # # print('num_graphs = ', batch_data.num_graphs)
+
+
+
+            # outs = [self.ggnn(ggnn_input_x[i], edge_index[i].long()) for i in range(batch_size)]
+            # node_mat = torch.stack(outs)
+            # print("GOT NODE_MAT!!!!!!!!!!!!", node_mat.shape)
+            # return
+            # node_mat = self.ggnn(initial_node_representation=input_dict["obs"]["state"], annotations=input_dict["obs"]["annotations"], adjacency_lists=input_dict["obs"]["adjacency_lists"])
+
+            # print("node_mat device = ", node_mat.device)
+            # print("-----------------AFTER GGNN-------------")
+            # print("node mat shape - ", node_mat.shape)
+            # return
+            
             input_state_list = node_mat
         else:
             input_state_list = initial_node_representation=input_dict["obs"]["state"]
@@ -161,7 +208,8 @@ class SelectNodeNetwork(TorchModelV2, nn.Module):
         inf_mask = torch.clamp(torch.log(mask), min=FLOAT_MIN)
         x = x + inf_mask
         # x = torch.where(mask, x, torch.tensor(masking_value).to(x.device))
-        #x = torch.where(mask, x, torch.tensor(FLOAT_MIN).to(x.device))        
+        #x = torch.where(mask, x, torch.tensor(FLOAT_MIN).to(x.device))
+        # print("Select node output and emb device", x.device, node_mat.device)        
         assert not torch.isnan(x).any(), "Nan in select node model output"
         return x, state, input_state_list
 
@@ -199,7 +247,9 @@ class ColorNetwork(TorchModelV2, nn.Module):
         
     def forward(self, input_dict, state, seq_lens):
         """Build a network that maps state -> action values."""
-        
+        # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # print("from colour node forward func : ", device)
+        # print("Colour  node input device", input_dict["obs"]["state"].device)
         x = F.relu(self.fc1(input_dict["obs"]["state"]))
         x = F.relu(self.fc2(x))
         x = torch.cat((x, input_dict["obs"]["node_properties"]), 1)
